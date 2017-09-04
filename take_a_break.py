a69136a57e8b9e57877fc385f2f7215a5f108382
Jerrin Varghese <jvargh81@lakers.mercyhurst.edu>
2017-09-03 23:08:24 -0400

    This program helps you to take break after certain intervals by letting you know, using some videos from youtube.
    Signed-off-by: Jerrin Varghese <jvargh81@lakers.mercyhurst.edu>

---
+++ take_a_break.py
@@ -0,0 +1,46 @@
+import webbrowser
+import time
+
+
+for i in range(0,3) :
+
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=kw4tT7SCmaY&list=RDEMsmTp6h47Ar7dkQlLDi6GYQ&index=27")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
